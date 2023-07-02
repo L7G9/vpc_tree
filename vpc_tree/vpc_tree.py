af13e62988ce4bf449cca7f7e8b8a237096f4aad
l7g9 <lukewgregory@gmail.com>
2023-07-02 16:01:03 +0100

    feat(vpc_tree): add functions to work out to prefix for a line in the tree

--- vpc_tree/vpc_tree.py
+++ vpc_tree/vpc_tree.py
@@ -4,7 +4,6 @@
 
 import boto3
 
-
 PIPE = "│"
 ELBOW = "└──"
 TEE = "├──"
@@ -12,34 +11,134 @@ PIPE_PREFIX = "│   "
 SPACE_PREFIX = "    "
 
 
-class VPCTree:
-  def __init__(self, vpc_arn):
-    self._generator = _TreeGenerator(vpc_arn)
+def _elbow_or_tree(last_node):
+    return ELBOW if last_node else TEE
 
-  def generate(self):
-    tree = self._generator.build_tree()
-    for entry in tree:
-      print(entry)
+
+def _space_or_pipe(last_node):
+    return SPACE_PREFIX if last_node else PIPE_PREFIX
+
+
+def _prefix(last_nodes):
+    prefix = ''
+
+    for i in range(len(last_nodes)):
+        leaf_node = i == len(last_nodes)-1
+        if leaf_node:
+            prefix += _elbow_or_tree(last_nodes[i])
+        else:
+            prefix += _space_or_pipe(last_nodes[i])
+
+    return prefix
+
+
+class VPCTree:
+    def __init__(self, vpc_id):
+        self._generator = _TreeGenerator(vpc_id)
+
+    def generate(self):
+        tree = self._generator.build_tree()
+        for entry in tree:
+            print(entry)
+
 
 class _TreeGenerator:
-  def __init__(self, vpc_arn):
-    self._tree[]
+    def __init__(self, vpc_id):
+        self._tree = []
+        self._vpc_id = vpc_id
+        self._client = boto3.client('ec2')
 
-  def build_tree(self):
-    self._vpc()
+    def build_tree(self):
+        self._vpc()
+        return self._tree
 
-  def _vpc(self):
-    # add details of vpc to tree
-    # for each subnet in vpc
-    #   _subnet()
-    pass
+    def _vpcs(self):
+        vpc_response = self._client.describe_vpcs()
+        for vpc in vpc_response['Vpcs']:
+            vpc_id = vpc['VpcId']
+            name = get_tag_value(vpc['Tags'], 'Name')
+            print(f"VPC : {name} : {vpc_id}")
 
-  def _subnet(self):
-    # add details of subnet to tree
-    # for each EC2 instance in subnet
-    #   _instance()
-    pass
+    def _vpc(self):
+        vpc_response = self._client.describe_vpcs(
+            VpcIds=[
+                self._vpc_id,
+            ],
+        )
+        vpc_id = vpc_response['Vpcs'][0]['VpcId']
+        cidr_block = vpc_response['Vpcs'][0]['CidrBlock']
 
-  def _instance(self):
-    # add details of instance to tree
-    pass
+        self._tree.append(f"{vpc_id} : {cidr_block}")
+        self._tree.append(PIPE)
+
+        subnet_response = self._client.describe_subnets(
+            Filters=[
+                {
+                    'Name': 'vpc-id',
+                    'Values': [
+                        vpc_id,
+                    ]
+                },
+            ]
+        )
+
+        subnet_count = len(subnet_response['Subnets'])
+        for i in range(subnet_count):
+            last_subnet = i == subnet_count-1
+            self._subnet(subnet_response['Subnets'][i], [last_subnet])
+
+    def _subnet(self, subnet, last_nodes):
+        subnet_id = subnet['SubnetId']
+        cidr_block = subnet['CidrBlock']
+
+        self._tree.append(f"{_prefix(last_nodes)} {subnet_id} : {cidr_block}")
+
+        instance_response = self._client.describe_instances(
+                Filters=[
+                    {
+                        'Name': 'subnet-id',
+                        'Values': [
+                            subnet_id,
+                        ]
+                    },
+                ]
+            )
+        for reservation in instance_response['Reservations']:
+            instance_count = len(reservation['Instances'])
+            for i in range(instance_count):
+                last_instance = i == instance_count-1
+                last_nodes = last_nodes.copy()
+                last_nodes.append(last_instance)
+                self._instance(reservation['Instances'][i], last_nodes)
+
+    def _instance(self, instance, last_nodes):
+        instance_id = instance['InstanceId']
+        prefix = _prefix(last_nodes)
+        self._tree.append(f"{prefix} {instance_id}")
+
+        security_group_count = len(instance['SecurityGroups'])
+        for i in range(security_group_count):
+            last_security_group = i == security_group_count-1
+            last_nodes = last_nodes.copy()
+            last_nodes.append(last_security_group)
+            self._security_group(instance['SecurityGroups'][i], last_nodes)
+
+    def _security_group(
+            self,
+            security_group,
+            last_nodes):
+        security_group_id = security_group['GroupId']
+        prefix = _prefix(last_nodes)
+        self._tree.append(f"{prefix} : {security_group_id}")
+
+
+def get_tag_value(tags, key):
+    for tag in tags:
+        if tag["Key"] == key:
+            return tag["Value"]
+    return ""
+
+
+if __name__ == "__main__":
+    vpc_tree = VPCTree("vpc-0493abc0802f1f4f9")
+    vpc_tree.generate()
