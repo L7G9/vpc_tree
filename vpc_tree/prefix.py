58180d67c8b86f3433a0788eee4eb42d22dcdcd5
l7g9 <lukewgregory@gmail.com>
2023-07-05 19:30:50 +0100

    refactor: move prefix functionality into own module

---
+++ vpc_tree/prefix.py
@@ -0,0 +1,34 @@
+# prefix.py
+
+"""This module provides VPC Tree main module."""
+
+PIPE = "│"
+ELBOW = "└──"
+TEE = "├──"
+PIPE_PREFIX = "│   "
+SPACE_PREFIX = "    "
+
+
+def elbow_or_tree(last_node):
+    return ELBOW if last_node else TEE
+
+
+def space_or_pipe(last_node):
+    return SPACE_PREFIX if last_node else PIPE_PREFIX
+
+
+def prefix(last_nodes):
+    prefix = ''
+
+    for i in range(len(last_nodes)):
+        leaf_node = i == len(last_nodes)-1
+        if leaf_node:
+            prefix += elbow_or_tree(last_nodes[i])
+        else:
+            prefix += space_or_pipe(last_nodes[i])
+
+    return prefix
+
+def prefix_list(prefix, list):
+    for item in list:
+        item = prefix + item
