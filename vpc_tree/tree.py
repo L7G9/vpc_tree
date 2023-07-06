17b70414a5ed428a60737d436a421ea0a900a6b4
l7g9 <lukewgregory@gmail.com>
2023-07-06 11:51:04 +0100

    refactor: add base tree class

---
+++ vpc_tree/tree.py
@@ -0,0 +1,17 @@
+# tree.py
+
+"""This module provides Tree main module."""
+
+from prefix import get_prefix
+
+
+class Tree:
+    def _tree_text(self, prefix_start, heading, items, item_function):
+        tree = []
+        tree.append(f"{get_prefix(prefix_start)}{heading}")
+        item_count = len(items)
+        for i in range(item_count):
+            last_item = i == item_count-1
+            tree += item_function(prefix_start + [last_item], items[i])
+
+        return tree
