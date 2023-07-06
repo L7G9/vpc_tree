8c3000d988afd9882eca7a1265a55d87a2a89779
l7g9 <lukewgregory@gmail.com>
2023-07-06 23:03:23 +0100

    refactor: move tag functions in own module

---
+++ vpc_tree/tags.py
@@ -0,0 +1,12 @@
+# tags.py
+
+
+def get_tag(tags, key):
+    return next(filter(lambda d: d.get("Key") == key, tags), None)
+
+
+def get_tag_value(tags, key):
+    tag = get_tag(tags, key)
+    if tag is not None:
+        return tag['Value']
+    return None
