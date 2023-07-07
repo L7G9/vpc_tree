a39d5864ebed767d96691161072461bdb9a50b25
l7g9 <lukewgregory@gmail.com>
2023-07-07 15:43:36 +0100

    refactor: connect subtrees using add_subtree_prefix

--- vpc_tree/vpc_tree.py
+++ vpc_tree/vpc_tree.py
@@ -4,51 +4,13 @@
 
 import boto3
 import pprint
-
+import prefix
+import tags
 from sg_tree import SGTree
 from subnet_tree import SubnetTree
 from lb_tree import LBTree
 from asg_tree import ASGTree
 from target_group_tree import TargetGroupTree
-
-PIPE = "│"
-ELBOW = "└──"
-TEE = "├──"
-PIPE_PREFIX = "│   "
-SPACE_PREFIX = "    "
-
-
-def _elbow_or_tree(last_node):
-    return ELBOW if last_node else TEE
-
-
-def _space_or_pipe(last_node):
-    return SPACE_PREFIX if last_node else PIPE_PREFIX
-
-
-def _prefix(last_nodes):
-    prefix = ''
-
-    for i in range(len(last_nodes)):
-        leaf_node = i == len(last_nodes)-1
-        if leaf_node:
-            prefix += _elbow_or_tree(last_nodes[i])
-        else:
-            prefix += _space_or_pipe(last_nodes[i])
-
-    return prefix
-
-
-def _get_tag(tags, key):
-    return next(filter(lambda d: d.get("Key") == key, tags), None)
-
-
-def _get_tag_value(tags, key):
-    tag = _get_tag(tags, key)
-    if tag is not None:
-        return tag['Value']
-    return None
-
 
 class VPCTree:
     def __init__(self, vpc_id):
@@ -75,7 +37,7 @@ class _TreeGenerator:
         vpc_response = self._client.describe_vpcs()
         for vpc in vpc_response['Vpcs']:
             vpc_id = vpc['VpcId']
-            name = _get_tag_value(vpc['Tags'], 'Name')
+            name = tags.get_tag_value(vpc['Tags'], 'Name')
             print(f"VPC : {name} : {vpc_id}")
 
     def _get_vpc(self, vpc_id):
@@ -90,7 +52,7 @@ class _TreeGenerator:
 
     def _get_vpc_description(self, vpc):
         vpc_id = vpc['VpcId']
-        name = _get_tag_value(vpc['Tags'], 'Name')
+        name = tags.get_tag_value(vpc['Tags'], 'Name')
         cidr_block = vpc['CidrBlock']
         return f"{vpc_id} : {name} : {cidr_block}"
 
@@ -100,25 +62,37 @@ class _TreeGenerator:
         self._tree.append(self._get_vpc_description(vpc))
 
         sg_tree = SGTree(self._vpc_id)
-        self._tree += sg_tree.generate()
+        sg_text_tree = sg_tree.generate()
+        prefix.add_subtree_prefix(sg_text_tree, prefix.TEE, prefix.PIPE_PREFIX)
+        self._tree += sg_text_tree
 
         subnet_tree = SubnetTree(self._vpc_id)
-        self._tree += subnet_tree.generate()
+        subnet_text_tree = subnet_tree.generate()
+        prefix.add_subtree_prefix(subnet_text_tree, prefix.TEE, prefix.PIPE_PREFIX)
+        self._tree += subnet_text_tree
 
         lb_tree = LBTree(self._vpc_id)
-        self._tree += lb_tree.generate()
+        load_balancer_text_tree = lb_tree.generate()
+        prefix.add_subtree_prefix(load_balancer_text_tree, prefix.TEE, prefix.PIPE_PREFIX)
+        self._tree += load_balancer_text_tree
 
         subnet_ids = []
         for subnet in subnet_tree.subnets:
             subnet_ids.append(subnet['SubnetId'])
+
         asg_tree = ASGTree(subnet_ids)
-        self._tree += asg_tree.generate()
+        auto_scaling_group_text_tree = asg_tree.generate()
+        prefix.add_subtree_prefix(auto_scaling_group_text_tree, prefix.TEE, prefix.PIPE_PREFIX)
+        self._tree += auto_scaling_group_text_tree
 
         load_balancer_arns = []
         for load_balancer in lb_tree.load_balancers:
             load_balancer_arns.append(load_balancer['LoadBalancerArn'])
+
         target_group_tree = TargetGroupTree(load_balancer_arns)
-        self._tree += target_group_tree.generate()
+        target_group_text_tree = target_group_tree.generate()
+        prefix.add_subtree_prefix(target_group_text_tree, prefix.ELBOW, prefix.SPACE_PREFIX)
+        self._tree += target_group_text_tree
 
 
 if __name__ == "__main__":
