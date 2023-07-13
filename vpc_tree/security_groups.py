80b71e15679fe932b5eb038d00660cd94d9ad3a6
l7g9 <lukewgregory@gmail.com>
2023-07-13 22:16:47 +0100

    docs: add docstrings

--- vpc_tree/security_groups.py
+++ vpc_tree/security_groups.py
@@ -6,16 +6,37 @@ import tree
 
 
 class SecurityGroups(tree.Tree):
+    """AWS Security Groups.
+
+    Subclass of tree.Tree with the functionality to get details of security
+       groups from boto3 and display them as a text based tree.
+
+    Attributes:
+        vpc_id: A string containing the id of a virtual private cloud.
+    """
     def __init__(self, vpc_id):
+        """Initializes instance.
+
+        Args:
+            vpc_id: A string containing the id virtual private cloud which all
+                the security groups should be linked to.
+        """
         self.vpc_id = vpc_id
 
     def generate(self):
+        """Generate a text based tree describing all security groups linked to
+            vpc_id.
+
+        Returns:
+            A list of strings containing the text based tree.
+        """
         sgs = self._get_sgs()
         return tree.Tree._tree_text(
             self, [], "Security Groups:", sgs, self._sg_text
         )
 
     def _get_sgs(self):
+        """Get all security groups linked to vpc_id using boto3."""
         sgs = []
 
         client = boto3.client("ec2")
@@ -32,6 +53,7 @@ class SecurityGroups(tree.Tree):
         return sgs
 
     def _sg_text(self, prefix, sg):
+        """Describe a security group as a list of strings."""
         id = sg["GroupId"]
         name = sg["GroupName"]
         return [f"{get_prefix(prefix)}{id} : {name}"]
