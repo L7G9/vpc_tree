eeebb0a2d5b0d11534a660ae998919dca1805637
l7g9 <lukewgregory@gmail.com>
2023-07-13 22:07:37 +0100

    docs: add docstrings

--- vpc_tree/auto_scaling_groups.py
+++ vpc_tree/auto_scaling_groups.py
@@ -8,10 +8,32 @@ import tree
 
 
 class AutoScalingGroups(tree.Tree):
+    """AWS Auto Scaling Groups.
+
+    Subclass of tree.Tree with the functionality to get details of auto
+        scaling groups from boto3 and display them as a text based tree.
+
+    Attributes:
+        subnet_ids: A list of strings containing the ids of the subnets in a
+            virtual private cloud.
+    """
+
     def __init__(self, subnet_ids):
+        """Initializes instance.
+
+        Args:
+            subnet_ids: A list of strings containing the ids of the subnets in
+                a virtual private cloud.
+        """
         self.subnet_ids = subnet_ids
 
     def generate(self):
+        """Generate a text based tree describing all the auto scaling groups
+            linked to subnets in subnet_ids.
+
+        Returns:
+            A list of strings containing the text based tree.
+        """
         asgs = self._get_asgs()
         asgs = self._filter_by_subnets(asgs, self.subnet_ids)
         return tree.Tree._tree_text(
@@ -19,6 +41,7 @@ class AutoScalingGroups(tree.Tree):
         )
 
     def _get_asgs(self):
+        """Get all ASGs using boto3."""
         asgs = []
 
         client = boto3.client("autoscaling")
@@ -31,6 +54,7 @@ class AutoScalingGroups(tree.Tree):
         return asgs
 
     def _filter_by_subnets(self, asgs, vpc_subnet_ids):
+        """Filter all ASGs by subnets in the virtual private cloud."""
         filtered_asgs = []
         for asg in asgs:
             asg_subnet_ids = asg["VPCZoneIdentifier"].split(",")
@@ -40,6 +64,7 @@ class AutoScalingGroups(tree.Tree):
         return filtered_asgs
 
     def _asg_text(self, prefix, asg):
+        """Describe an ASG as a list of strings."""
         text_tree = []
 
         asg_arn = asg["AutoScalingGroupARN"]
@@ -67,7 +92,9 @@ class AutoScalingGroups(tree.Tree):
         return text_tree
 
     def _subnet_text(self, prefix, subnet_id):
+        """Describe a subnet linked to an ASG as a list of strings."""
         return [f"{get_prefix(prefix)} {subnet_id}"]
 
     def _instance_text(self, prefix, instance):
+        """Describe an instance linked to an ASG as a list of strings."""
         return [f"{get_prefix(prefix)} {instance['InstanceId']}"]
