a0b49f060e027769fa8289928ade6c2055b2ac97
l7g9 <lukewgregory@gmail.com>
2023-07-13 23:39:13 +0100

    feat: use VPCTree class to process options

--- vpc_tree/cli.py
+++ vpc_tree/cli.py
@@ -3,14 +3,17 @@
 
 import argparse
 from . import __version__
-#from .vpc_tree import
+from . import vpc_tree
 
 
 def main():
     args = parse_cmd_line_arguments()
-    print(args)
-    print(f"list-vpcs {args.list_vpcs}")
-    print(f"vpc {args.vpc_id}")
+    tree = vpc_tree.VPCTree()
+    if args.list_vpcs:
+        tree.display_vpc_list()
+    else:
+        tree.display_vpc_tree(args.vpc_id)
+
 
 def parse_cmd_line_arguments():
     parser = argparse.ArgumentParser(
@@ -24,13 +27,13 @@ def parse_cmd_line_arguments():
         "-l",
         "--list-vpcs",
         action="store_true",
-        help="Generate a list of VPCs"
+        help="Generate a list of VPCs",
     )
     parser.add_argument(
         "vpc_id",
         metavar="VPC_ID",
         nargs="?",
-        help="Generate a tree describing the structure of the virtual Private Cloud with VPD_ID"
+        help="Generate a tree describing the structure of the virtual Private Cloud with VPD_ID",
     )
 
     return parser.parse_args()
