b425c3cce8474c74b567aa72ad26c524d9be0752
l7g9 <lukewgregory@gmail.com>
2023-07-14 10:36:59 +0100

    docs: add dockstrings

--- vpc_tree/target_groups.py
+++ vpc_tree/target_groups.py
@@ -6,10 +6,31 @@ from . import tree
 
 
 class TargetGroups(tree.Tree):
+    """AWS Target Groups.
+
+    Subclass of tree.Tree with the functionality to get details of target
+        groups from boto3 and display them as a text based tree.
+
+    Attributes:
+        load_balancer_arns: A list of strings containing the arns of the load balancers in a
+            virtual private cloud.
+    """
     def __init__(self, load_balancer_arns):
+        """Initializes instance.
+
+        Args:
+            load_balancer_arns: A list of strings containing the arns of the load balancers in
+                a virtual private cloud.
+        """
         self.load_balancer_arns = load_balancer_arns
 
     def generate(self):
+        """Generate a text based tree describing all the target groups linked to the load balancers
+               in a virtual private cloud.
+
+        Returns:
+            A list of strings containing the text based tree.
+        """
         target_groups = self._get_target_groups(self.load_balancer_arns)
 
         return tree.Tree._tree_text(
@@ -17,6 +38,7 @@ class TargetGroups(tree.Tree):
         )
 
     def _get_target_groups(self, load_balancer_arns):
+        """Get all target groups linked to load balancer arns using boto3."""
         target_groups = []
 
         client = boto3.client("elbv2")
@@ -30,6 +52,7 @@ class TargetGroups(tree.Tree):
         return target_groups
 
     def _target_group_text(self, prefix_list, target_group):
+        """Describe target group as a list os strings."""
         text_tree = []
 
         arn = target_group["TargetGroupArn"]
@@ -50,4 +73,5 @@ class TargetGroups(tree.Tree):
         return text_tree
 
     def _load_balancer_text(self, prefix_list, load_balancer_arn):
+        """Describe load balancer linked to target group as a list of strings."""
         return [f"{get_prefix(prefix_list)}{load_balancer_arn}"]
