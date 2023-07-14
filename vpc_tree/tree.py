e975de221d8404cac6d7dd69df8bba7b9a604379
l7g9 <lukewgregory@gmail.com>
2023-07-14 11:29:03 +0100

    docs: add docstrings

--- vpc_tree/tree.py
+++ vpc_tree/tree.py
@@ -6,7 +6,18 @@ from .prefix import get_prefix
 
 
 class Tree:
+    """Tree class to be inherited by classes creating text based trees from AWS resources."""
     def _tree_text(self, prefix_start, heading, items, item_function):
+        """Create a text based tree.
+        Args:
+            prefix_start: A list of booleans describing a common prefix to be added to all strings is this text tree.
+            heading: A string containing a heading to give this tree.
+            items: A list of dictionaries containing AWS resources.
+            item_function: a function that takes a dictionary from items and describes it as a list of strings.
+
+        Returns:
+            A list of strings containing the text based tree.
+        """
         tree = []
         tree.append(f"{get_prefix(prefix_start)}{heading}")
         item_count = len(items)
