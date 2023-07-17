c027201fe22bc2a263326c9a3a045e82f0cbe119
l7g9 <lukewgregory@gmail.com>
2023-07-17 15:46:05 +0100

    build: update version and change log

--- vpc_tree/__init__.py
+++ vpc_tree/__init__.py
@@ -2,8 +2,15 @@
 """Top-level package for VPC Tree.
 
 Change Log.
+- 0.2.1 Expand information in Auto Scaling Group sub-tree to include...
+    - Launch Template
+    - Launch Configurations
+    - MixedInstancesPolicy
+    - Min and Max Size
+    - Load Balancers
+    - Target Groups
 - 0.2.0 Security Group Permissions added.
-- 0.1.0 Initial release
+- 0.1.0 Initial release.
 """
 
-__version__ = "0.2.0"
+__version__ = "0.2.1"
