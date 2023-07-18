026b8359f66b3b649cec1d587a91bea210d42b6d
l7g9 <lukewgregory@gmail.com>
2023-07-18 21:09:12 +0100

    refactor(ASGTree): update to use add_node and add_tree

--- vpc_tree/asg_tree.py
+++ vpc_tree/asg_tree.py
@@ -4,7 +4,7 @@
 import boto3
 
 from .prefix import get_prefix
-from .text_tree import generate_tree
+from .text_tree import add_node, add_tree
 
 
 class ASGTree:
@@ -25,15 +25,22 @@ class ASGTree:
         """
         self.subnet_ids = subnet_ids
 
-    def generate(self):
+    def generate(self, text_tree, prefix_description):
         """Generate a text based tree describing all the Auto Scaling Groups
         linked to the Subnets in subnet_ids.
 
-        Returns:
-            A list of strings containing the text based tree.
+        Args:
+            text_tree: A list of strings to add this subtree to.
+            prefix_description: A list of booleans describing a common prefix
+            to be added to all strings is this text tree.
         """
-        asgs = self._filter_by_subnets(self._get_asgs(), self.subnet_ids)
-        return generate_tree([], "Auto Scaling Groups:", asgs, self._asg_text)
+        add_tree(
+            text_tree,
+            prefix_description,
+            "Auto Scaling Groups:",
+            self._filter_by_subnets(self._get_asgs(), self.subnet_ids),
+            self._add_asg_tree,
+        )
 
     def _get_asgs(self):
         """Get all Auto Scaling Groups using Boto3."""
@@ -59,10 +66,8 @@ class ASGTree:
 
         return filtered_asgs
 
-    def _asg_text(self, prefix_description, asg):
-        """Describe an Auto Scaling Group as a list of strings."""
-        text_tree = []
-
+    def _add_asg_tree(self, text_tree, prefix_description, asg):
+        """Adds tree describing Auto Scaling Group to text_tree."""
         arn = asg["AutoScalingGroupARN"]
         name = asg["AutoScalingGroupName"]
         text_tree.append(f"{get_prefix(prefix_description)}{arn} : {name}")
@@ -90,56 +95,40 @@ class ASGTree:
             ]["LaunchTemplateId"]
             text_tree.append(f"{sub_prefix_2}{id}")
 
-        subnet_tree = generate_tree(
+        add_tree(
+            text_tree,
             prefix_description + [False],
             "Subnets:",
             asg["VPCZoneIdentifier"].split(","),
-            self._subnet_text,
+            add_node,
         )
-        text_tree += subnet_tree
 
-        instance_tree = generate_tree(
+        add_tree(
+            text_tree,
             prefix_description + [False],
             "Instances:",
             asg["Instances"],
-            self._instance_text,
+            self._add_instance_node,
         )
-        text_tree += instance_tree
 
-        lb_tree = generate_tree(
+        add_tree(
+            text_tree,
             prefix_description + [False],
             "Load Balancers:",
             asg["LoadBalancerNames"],
-            self._lb_text,
+            add_node,
         )
-        text_tree += lb_tree
 
-        tg_tree = generate_tree(
+        add_tree(
+            text_tree,
             prefix_description + [True],
             "Target Groups:",
             asg["TargetGroupARNs"],
-            self._tg_text,
+            add_node,
         )
-        text_tree += tg_tree
 
-        return text_tree
-
-    def _subnet_text(self, prefix_description, subnet_id):
-        """Describe a Subnet linked to an Auto Scaling Group as a list of
-        strings."""
-        return [f"{get_prefix(prefix_description)}{subnet_id}"]
-
-    def _instance_text(self, prefix_description, instance):
-        """Describe an Instance linked to an Auto Scaling Group as a list of
-        strings."""
-        return [f"{get_prefix(prefix_description)}{instance['InstanceId']}"]
-
-    def _lb_text(self, prefix_description, lb_name):
-        """Describe a Load Balancer linked to an Auto Scaling Group as a list
-        of strings."""
-        return [f"{get_prefix(prefix_description)}{lb_name}"]
-
-    def _tg_text(self, prefix_description, tg_arn):
-        """Describe a Target Group linked to an Auto Scaling Group as a list of
-        strings."""
-        return [f"{get_prefix(prefix_description)}{tg_arn}"]
+    def _add_instance_node(self, text_tree, prefix_description, instance):
+        """Adds Id of Instance linked to Auto Scaling Group to text_tree."""
+        text_tree.append(
+            f"{get_prefix(prefix_description)}{instance['InstanceId']}"
+        )
