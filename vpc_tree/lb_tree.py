d84317f2fda911b2e557099d6860418bf817039a
l7g9 <lukewgregory@gmail.com>
2023-07-19 12:12:59 +0100

    refactor(LBTree): update to use add_node and add_tree

--- vpc_tree/lb_tree.py
+++ vpc_tree/lb_tree.py
@@ -4,7 +4,7 @@
 import boto3
 
 from .prefix import get_prefix
-from .text_tree import generate_tree
+from .text_tree import add_node, add_tree
 
 
 class LBTree:
@@ -27,17 +27,23 @@ class LBTree:
         self.vpc_id = vpc_id
         self.load_balancers = []
 
-    def generate(self):
+    def generate(self, text_tree, prefix_description):
         """Generate a text based tree describing all Load Balancers linked to
         vpc_id.
 
-        Returns:
-            A list of strings containing the text based tree.
+        Args:
+            text_tree: A list of strings to add this subtree to.
+            prefix_description: A list of booleans describing a common prefix
+            to be added to all strings is this text tree.
         """
         self.load_balancers = self._filter_by_vpc(self._get_lbs(), self.vpc_id)
 
-        return generate_tree(
-            [], "Load Balancers:", self.load_balancers, self._lb_text
+        add_tree(
+            text_tree,
+            prefix_description,
+            "Load Balancers:",
+            self.load_balancers,
+            self._add_lb_tree,
         )
 
     def _get_lbs(self):
@@ -57,39 +63,33 @@ class LBTree:
         """Filter Load Balancers by Virtual Private Cloud Id."""
         return list(filter(lambda d: d["VpcId"] == vpc_id, lbs))
 
-    def _lb_text(self, prefix_description, lb):
-        """Describe a load balancer as a list of strings."""
-        text_tree = []
+    def _add_lb_tree(self, text_tree, prefix_description, lb):
+        """Adds tree describing Load Balancer to text_tree."""
         arn = lb["LoadBalancerArn"]
         name = lb["LoadBalancerName"]
         text_tree.append(f"{get_prefix(prefix_description)}{arn} : {name}")
 
-        az_tree = generate_tree(
+        add_tree(
+            text_tree,
             prefix_description + [False],
             "Availability Zones:",
             lb["AvailabilityZones"],
-            self._az_text,
+            self._add_az_node,
         )
-        text_tree += az_tree
 
-        sg_tree = generate_tree(
+        add_tree(
+            text_tree,
             prefix_description + [True],
             "Security Groups:",
             lb["SecurityGroups"],
-            self._sg_text,
+            add_node,
         )
-        text_tree += sg_tree
 
-        return text_tree
-
-    def _az_text(self, prefix_description, az):
-        """Describe an Availability Zone linked to a Load Balancer as a list of
-        strings."""
+    def _add_az_node(self, text_tree, prefix_description, az):
+        """Adds details of Availability Zone linked to a Load Balancer to
+        text_tree."""
         zone = az["ZoneName"]
         subnet_id = az["SubnetId"]
-        return [f"{get_prefix(prefix_description)}{zone} : {subnet_id}"]
-
-    def _sg_text(self, prefix_description, sg):
-        """Describe a Security Group linked to a Load Balancer as a list of
-        strings."""
-        return [f"{get_prefix(prefix_description)}{sg}"]
+        text_tree.append(
+            f"{get_prefix(prefix_description)}{zone} : {subnet_id}"
+        )
