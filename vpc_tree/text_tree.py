a445b03aca648c6808ec1e05326581ccfe27aa56
l7g9 <lukewgregory@gmail.com>
2023-07-19 13:07:43 +0100

    refactor(text_tree.py): replace generate_tree with add_tree and add_node

--- vpc_tree/text_tree.py
+++ vpc_tree/text_tree.py
@@ -1,27 +1,35 @@
 # text_tree.py
-"""Help function to create a generic text based tree."""
+"""Helper functions to create a generic text based tree."""
 
 from .prefix import get_prefix
 
 
-def generate_tree(prefix_description, heading, items, item_function):
-    """Create a text based tree.
+def add_tree(text_tree, prefix_description, heading, items, item_function):
+    """Add tree description to a list of strings.
+
     Args:
+        text_tree: A list of strings to add this tree to.
         prefix_description: A list of booleans describing a common prefix to
         be added to all strings is this text tree.
         heading: A string containing a heading to give this tree.
         items: A list of dictionaries containing AWS resources.
         item_function: a function that takes a dictionary from items and
         describes it as a list of strings.
-
-     Returns:
-        A list of strings containing the text based tree.
     """
-    tree = []
-    tree.append(f"{get_prefix(prefix_description)}{heading}")
+    text_tree.append(f"{get_prefix(prefix_description)}{heading}")
     item_count = len(items)
     for i in range(item_count):
         last_item = i == item_count - 1
-        tree += item_function(prefix_description + [last_item], items[i])
+        item_function(text_tree, prefix_description + [last_item], items[i])
 
-    return tree
+
+def add_node(text_tree, prefix_description, string):
+    """Add node description to a list of strings.
+
+    Args:
+        text_tree: A list of strings to add this string to.
+        prefix_description: A list of booleans describing the prefix to be
+        added to this node.
+        string: A string describing the node.
+    """
+    text_tree.append(f"{get_prefix(prefix_description)}{string}")
