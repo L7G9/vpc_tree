ce65dc3b1a23bc17b5890aec6ec9966bfbe33b52
l7g9 <lukewgregory@gmail.com>
2023-07-20 04:55:07 +0100

    refactor(TGTree): remove _get_tgs

--- vpc_tree/tg_tree.py
+++ vpc_tree/tg_tree.py
@@ -1,29 +1,26 @@
 # tg_tree.py
 """VPC Tree application's Target Group functionality."""
 
-import boto3
-
 from .prefix import get_prefix
 from .text_tree import add_node, add_tree
 
 
 class TGTree:
-    """Gets details of AWS Target Groups and represents them in a tree
-    structure.
+    """Generates a text tree representation of AWS Target Groups.
 
     Attributes:
-        load_balancer_arns: A list of strings containing the arns of the Load
-        Balancers in a Virtual Private Cloud.
+        target_groups: A list of dictionaries containing Target Groups from
+        Boto3.
     """
 
-    def __init__(self, load_balancer_arns):
+    def __init__(self, target_groups):
         """Initializes instance.
 
         Args:
-            load_balancer_arns: A list of strings containing the arns of the
-            Load Balancers in a Virtual Private Cloud.
+            target_groups: A list of dictionaries containing Target Groups from
+            Boto3.
         """
-        self.load_balancer_arns = load_balancer_arns
+        self.target_groups = target_groups
 
     def generate(self, text_tree, prefix_description):
         """Generate a text based tree describing all the Target Groups linked
@@ -39,23 +36,9 @@ class TGTree:
             text_tree,
             prefix_description,
             "Target Groups:",
-            self._get_target_groups(self.load_balancer_arns),
+            self.target_groups,
             self._add_tg_tree,
         )
-
-    def _get_target_groups(self, load_balancer_arns):
-        """Get all target groups linked to load balancer arns using Boto3."""
-        target_groups = []
-
-        client = boto3.client("elbv2")
-        paginator = client.get_paginator("describe_target_groups")
-
-        for arn in load_balancer_arns:
-            page_iterator = paginator.paginate(LoadBalancerArn=arn)
-            for page in page_iterator:
-                target_groups += page["TargetGroups"]
-
-        return target_groups
 
     def _add_tg_tree(self, text_tree, prefix_description, target_group):
         """Adds tree describing Target Group to text_tree."""
